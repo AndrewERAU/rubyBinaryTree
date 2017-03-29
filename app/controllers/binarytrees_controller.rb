4ab22e6437f60f985ca0ca01a7f6f71ed8762a97
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-03-29 14:46:18 -0700

    Added timing for each operation

--- app/controllers/binarytrees_controller.rb
+++ app/controllers/binarytrees_controller.rb
@@ -1,6 +1,8 @@
 require_relative  'binaryTreeClass' # this was needed b/c global $binaryTree was null
 class BinarytreesController < ApplicationController
+   # TODO: Add timing info and print out time taken for each operation in view
    def create
+      t1 = Time.now.to_f
       if params[:commit] == 'Insert'
          $binaryTree.insert(params[:binarytree][:title].to_i) # to_i converts string to int
       elsif params[:commit] == 'Search'
@@ -8,7 +10,9 @@ class BinarytreesController < ApplicationController
       elsif params[:commit] == 'Remove'
          $binaryTree.remove($binaryTree.head,nil,params[:binarytree][:title].to_i)
       end
-
+      t2 = Time.now.to_f
+      time_ms = (t2-t1) * 1000
+      $binaryTree.result = $binaryTree.result + " - " + (time_ms.round(2)).to_s + "ms"
       redirect_to home_index_url
    end
 
